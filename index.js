5b2766b1a6193c3db3d35cdf96224094386fa9ad
rzrgames <110288747+rzrgames@users.noreply.github.com>
2024-05-04 01:52:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ router.get('/', function (req, res, next) {
     console.log('rputer calld')
     let url = "https://youtu.be/nD_NDngrEl8";
     ytdl(url).pipe(fs.createWriteStream('video.mp4'));
-    res.end();
+    res.end(url);
 })
 app.use(router);
 app.listen(3000)
