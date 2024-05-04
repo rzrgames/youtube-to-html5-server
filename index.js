b4c4685f6e1734bcce3cd84455a9c1d3858ed7f4
rzrgames <110288747+rzrgames@users.noreply.github.com>
2024-05-05 02:25:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
-const fs = require('fs');
 const app = express();
 app.use(cors());
 
@@ -10,27 +9,6 @@ app.use(cors());
 
 
 
-
-
-const ytdlOptions = {
-    requestOptions: {
-        maxRetries: 5,
-        backoff: {inc: 2000, max: 2000},
-        transform: (parsed) => {
-            const originURL = parsed.protocol + "//" + parsed.hostname + parsed.path;
-            parsed.host = "proxy.darenliang.com";
-            parsed.hostname = "proxy.darenliang.com";
-            parsed.path = "/?url=" + encodeURIComponent(originURL);
-            parsed.protocol = "https:";
-            return parsed;
-        }
-    }
-};
-
-const getInfo = async url => {
-    console.log("[info] getting info");
-    return await ytdl.getInfo(url, ytdlOptions);
-};
 
 
 
@@ -59,7 +37,7 @@ app.get('/', (req,res) => {
     var URL = req.query.URL;
   
 	try {
-const info = getInfo(URL);
+
 				//res.end(JSON.stringify(ytdl.getInfo(URL, ytdlOptions)));
 				
 				const iTag = 134;
@@ -70,6 +48,7 @@ const info = getInfo(URL);
 });
 				
 				
+
 //res.end(JSON.stringify(video));
 
 					ytdl.getInfo(URL).then(data => {
