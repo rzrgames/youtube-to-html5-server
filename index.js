e57fdb8f8eaf5abb4f1b28eecf0372af99aa2248
rzrgames <110288747+rzrgames@users.noreply.github.com>
2024-05-05 05:57:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,68 +3,13 @@ const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
 app.use(cors());
-
-
-// Remove 'user:pass@' if you don't need to authenticate to your proxy.
-
-
-
-
-
-
-
-
-function sendResponse(response, data) {
-	response.end(JSON.stringify(data));
-}
-
-
-/**
- * Send a failed response.
- *
- * @param {ServerResponse} response
- * @param {*} data
- */
-function sendError(response, data) {
-	sendResponse(response, {
-		success: false,
-		data
-	});
-}
-
-
-app.get('/', (req,res) => {
-    var URL = req.query.URL;
-  
-	try {
-
-				//res.end(JSON.stringify(ytdl.getInfo(URL, ytdlOptions)));
-				
-				const iTag = 134;
-				const video = ytdl(URL, {
-  filter: function (format) {
-    return format.itag === iTag;
-  },
-});
-				
-				
-
-//res.end(JSON.stringify(video));
-
-					ytdl.getInfo(URL).then(data => {
-					
-					res.end(JSON.stringify(data));
-				}).catch(error => {
-				
-					sendError(res, error);
-				});
-
-			} catch (error) {
-				sendError(res, error);
-			}
-
-});
-
 app.listen(4000, () => {
     console.log('Server Works !!! At port 4000');
+});
+app.get('/download', (req,res) => {
+var URL = req.query.URL;
+res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+ytdl(URL, {
+    format: 'mp4'
+    }).pipe(res);
 });
