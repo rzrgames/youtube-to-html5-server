7b3476456362c2b38335a0dea5c98456fa200b61
rzrgames <110288747+rzrgames@users.noreply.github.com>
2024-05-22 01:12:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,24 @@ const port = 3000;
 const successCacheAge = 14400; // 4hrs
 const errorCacheAge = 3600; // 1hrs
 
+const proxy = url => {
+    return "https://your-worker.jdsjeo.workers.dev/?url=" + encodeURIComponent(url);
+};
 
+const ytdlOptions = {
+    requestOptions: {
+        maxRetries: 5,
+        backoff: {inc: 2000, max: 2000},
+        transform: (parsed) => {
+            const originURL = parsed.protocol + "//" + parsed.hostname + parsed.path;
+            parsed.host = "your-worker.jdsjeo.workers.dev";
+            parsed.hostname = "your-worker.jdsjeo.workers.dev";
+             parsed.path = "/?url=" + encodeURIComponent(originURL);
+            parsed.protocol = "https:";
+            return parsed;
+        }
+    }
+};
 /**
  * Send response headers and data.
  *
@@ -16,8 +33,13 @@ const errorCacheAge = 3600; // 1hrs
  * @param {object} data
  */
 function sendResponse(response, data) {
+	response.setHeader('Access-Control-Allow-Origin', '*');
+	response.setHeader('Access-Control-Allow-Methods', 'GET');
+
+	response.setHeader( 'Content-Type', 'application/json' );
 	response.end(JSON.stringify(data));
 }
+
 
 
 /**
@@ -46,12 +68,6 @@ function sendError(response, data) {
 	});
 }
 
-
-
-
-
-
-
 //create a server object:
 http.createServer(function(request, response) {
 	const queryObject = url.parse(request.url, true).query;
@@ -75,9 +91,9 @@ http.createServer(function(request, response) {
 				/**
 				 * @link https://github.com/fent/node-ytdl-core
 				 */
-				ytdl.getInfo(youtubeUrl).then(data => {
-					
-					response.end(JSON.stringify(data));
+				ytdl.getInfo(youtubeUrl, ytdlOptions).then(data => {
+						sendSuccess(response, data);
+					//response.end(JSON.stringify(data));
 				}).catch(error => {
 				
 					sendError(response, error);
